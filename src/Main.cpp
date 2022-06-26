cb09f1a3dfd586108c16cf47f494c6adb5e0ead9
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2022-06-26 00:42:44 -0400

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,40 @@
+#include <Stardust-Celeste.hpp>
+
+using namespace Stardust_Celeste;
+
+/**
+ * @brief Class to run the game state
+ *
+ */
+class GameApplication : public Core::Application
+{
+public:
+    /**
+     * @brief On Start override
+     */
+    void on_start() override
+    {
+        SC_APP_INFO("RUNNING!");
+    }
+
+private:
+};
+
+/**
+ * @brief Create a New Stardust Celeste Application object
+ *
+ * @return Core::Application*
+ */
+Core::Application *CreateNewSCApp()
+{
+
+    // Configure the engine
+    Core::AppConfig config;
+    config.headless = true;
+    config.networking = true;
+
+    Core::PlatformLayer::get().initialize(config);
+
+    // Return the game
+    return new GameApplication();
+}
