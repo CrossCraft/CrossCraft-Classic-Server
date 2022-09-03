95ca578b97124ec4d4a78d38d8fe81a64f2a2452
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2022-09-03 15:11:05 -0400

    Net setup

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,12 @@
 const std = @import("std");
+const network = @import("network");
+
+pub const io_mode = .evented;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("CrossCraft Classic Server v1.3\n", .{});
+    std.debug.print("Initializing Network...\n", .{});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    try network.init();
+    defer network.deinit();
 }
