a95ab3056f897766d542cbc5a9bfe59988c2aa7d
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2022-09-03 14:48:07 -0400

    Remove

--- src/World/Generation/WorldGenUtil.hpp
+++
@@ -1,9 +0,0 @@
-#include "../World.hpp"
-
-namespace ClassicServer {
-class World;
-struct WorldGenUtil {
-    static auto make_tree(World *wrld, int x, int z, int h) -> void;
-    static auto make_tree2(World *wrld, int x, int z, int h) -> void;
-};
-} // namespace ClassicServer
