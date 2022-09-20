e1b11bfc81bb920aba817da71bdab768b0acbd5e
Nathan Bourgeois <iridecentrosesfall@gmail.com>
2022-09-20 05:16:07 -0400

    Ping Broadcast, Client Array, Garbage Collected Clients

--- src/server.zig
+++ src/server.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const network = @import("network");
 const Client = @import("client.zig");
 const world = @import("world.zig");
+const protocol = @import("protocol.zig");
 
 /// Fixed Buffer Allocation
 var ram_buffer: [16 * 1000 * 1000]u8 = undefined;
@@ -11,6 +12,9 @@ var fba: std.heap.FixedBufferAllocator = undefined;
 /// Server Socket
 var socket: network.Socket = undefined;
 var ticks_alive: usize = 0;
+
+// ID 0 is Reserved for Console
+var client_list: [128]?*Client = undefined;
 
 /// Initialize Server
 pub fn init() !void {
@@ -22,10 +26,18 @@ pub fn init() !void {
     fba = std.heap.FixedBufferAllocator.init(&ram_buffer);
     allocator = fba.allocator();
 
+    // Init world
     try world.init(&allocator);
+
+    // Init client list
+    var i : usize = 0;
+    while(i < 128) : (i += 1) {
+        client_list[i] = null;
+    }
 
     // Create brand new socket handle
     socket = try network.Socket.create(.ipv4, .tcp);
+    try socket.enablePortReuse(true);
 
     // Bind socket to port
     try socket.bind(.{
@@ -38,6 +50,54 @@ pub fn init() !void {
     std.debug.print("Listening for connections...\n", .{});
 }
 
+/// Gets an ID if available -- returns -1 otherwise
+fn get_available_ID() i8 {
+    var i : usize = 1;
+
+    while(i < 128) : (i += 1){
+        if(client_list[i] == null)
+            return @intCast(i8, i);
+    }
+
+    return -1;
+}
+
+fn ping_all() !void {
+    var buf = try protocol.create_packet(&allocator, protocol.Packet.Ping);
+    protocol.make_ping(buf);
+    defer allocator.free(buf);
+    
+    var i : usize = 1;
+    while(i < 128) : (i += 1){
+        if(client_list[i] != null){
+            var client = client_list[i].?;
+            client.send(buf);        
+        }
+    }
+}
+
+fn gc_dead_clients() void {
+    var i : usize = 1;
+    var kills : usize = 0;
+
+    while(i < 128) : (i += 1){
+        if(client_list[i] != null) {
+            if(!client_list[i].?.is_connected){
+                var client = client_list[i].?;
+                client.deinit();
+                allocator.destroy(client);
+                client_list[i] = null;
+
+                kills += 1;
+            }
+        }
+    }
+
+    if(kills > 0){
+        std.debug.print("Killed {} dead connections.\n", .{kills});
+    }
+}
+
 /// Run Server
 pub fn run() !void {
     while (true) {
@@ -47,8 +107,15 @@ pub fn run() !void {
         world.update();
         ticks_alive += 1;
 
-        // TODO: GC dead clients, unbind from array
-        // TODO: Update All Clients Based on World (broadcast packets, etc.)
+        // PING ALL CLIENTS
+        if(ticks_alive % 200 == 0) {
+            try ping_all();
+
+            // Garbage Collect Dead Clients
+            gc_dead_clients();
+        }
+
+        // TODO: Update All Clients Based on World Events (broadcast packets, etc.)
 
         // Check Client
         var conn = socket.accept() catch |err| switch (err) {
@@ -59,8 +126,13 @@ pub fn run() !void {
         std.debug.print("New Client @ {}\n", .{try conn.getRemoteEndPoint()});
 
         // New Client Thread
-        // TODO: Assign ID number OR kick for full server
-        // TODO: Add to Client* array
+        var id : i8 = get_available_ID();
+
+        if(id < 0){
+            conn.close();
+            continue;
+        }
+
         const client = try allocator.create(Client);
         client.* = Client{
             .conn = conn,
@@ -70,20 +142,30 @@ pub fn run() !void {
             .packet_buffer = undefined,
             .username = undefined,
             .is_loaded = false,
-            .kick_max = false,
             .is_op = 0,
             .yaw = 0,
             .pitch = 0,
             .x = 0,
             .y = 0,
             .z = 0,
-            .id = 1,
+            .id = @bitCast(u8, id),
         };
+        client_list[@intCast(usize, @bitCast(u8, id))] = client;
     }
 }
 
 /// Cleanup Server
 pub fn deinit() void {
+    var i : usize = 1;
+    while(i < 128) : (i += 1){
+        if(client_list[i] != null) {
+            var client = client_list[i].?;
+            client.deinit();
+            allocator.destroy(client);
+            client_list[i] = null;
+        }
+    }
+
     socket.close();
     network.deinit();
     world.deinit();
